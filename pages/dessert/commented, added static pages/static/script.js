badf3c3b2b6d45d22b730e59e27b9d417247977e
Wing Ki Au <wingki.au@jjay.cuny.edu>
2022-01-13 08:19:11 -0500

    new pages for backend
    added {%static}

---
+++ pages/dessert/commented, added static pages/static/script.js
@@ -0,0 +1,22 @@
+ // func for heart icon
+ function changeHeart() {
+    // initializing var heartId to 'heart' id in html file
+     var heartId = document.getElementById("heart");
+     // if src of heart img is regular then change to solid
+     if (heartId.src.match("heart-regular.svg")) {    
+       heartId.src = "heart-solid.svg";
+     } else {
+       // change back to regular
+       heartId.src = "heart-regular.svg";
+     }
+   }
+   
+   var button = document.getElementById("button");
+   button.addEventListener("click", function () {
+     var value = document.getElementById("comment_box").value;
+     var li = document.createElement("li");
+     var text = document.createTextNode(value);
+     li.appendChild(text);
+     document.getElementById("unordered").appendChild(li);
+    
+   });
