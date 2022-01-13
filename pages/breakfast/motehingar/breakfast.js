bea48527e704a90f05ab79f348ba3d029344256a
codingsake <thinthazin.chris@gmail.com>
2022-01-12 21:16:12 -0500

    MoteHinGar_Breakfast

---
+++ pages/breakfast/motehingar/breakfast.js
@@ -0,0 +1,48 @@
+ // func for heart icon
+function changeHeart() {
+ // initializing var heartId to 'heart' id in html file
+  var heartId = document.getElementById("heart");
+  // if src of heart img is regular then change to solid
+  if (heartId.src.match("images/heart-regular.svg")) {    
+    heartId.src = "images/heart-solid.svg";
+  } else {
+    // change back to regular
+    heartId.src = "images/heart-regular.svg";
+  }
+}
+// func for comment icon
+function changeComment() {
+  // initializing var commentId to 'comment' id in html file
+  var commentId = document.getElementById("comment");
+  // if src of comment img is regular then change to solid
+  if (commentId.src.match("images/comment-regular.svg")) {   
+    commentId.src = "images/comment-solid.svg";
+  } else {
+    // change back to regular
+    commentId.src = "images/comment-regular.svg";
+  }
+}
+//icon not solid in another click
+function show_n_hide() {
+  if (document.getElementById("dialog").style.display == "none") {
+    document.getElementById("dialog").style.display = "block";
+  } else {
+    document.getElementById("dialog").style.display = "none";
+  }
+  return false;
+}
+var postBtn = document.getElementById("postBtn");
+// using an event listener so that whenever I click...func is called
+postBtn.addEventListener("click", function () {
+  // initializing var value to return the value the user types
+  var value = document.getElementById("comment_box").value;
+ // initializing var li to new element 'li'
+  var li = document.createElement("li");
+   // creating a node for every text. text is the node...value is the data
+  var text = document.createTextNode(value);
+ // appending input text to our newly created list (li)
+  li.appendChild(text);
+   // lastly...appending the list to an unordered list
+  document.getElementById("unordered_list").appendChild(li);
+ 
+});
